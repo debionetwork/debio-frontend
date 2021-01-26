c78c481f23cef2048b84fe3494558a6694186c91
Kevin Janada <kevin@Kevins-MacBook-Air.local>
2021-01-26 09:59:12 +0700

    Show rejection reason in: - Order History table - Lab Dashboard processing page

--- src/views/Dashboard/Lab/Process/RejectAlert.vue
+++ src/views/Dashboard/Lab/Process/RejectAlert.vue
@@ -16,15 +16,55 @@
         </div>
       </v-card-title>
       <v-card-text class="px-8">
-        TODO: Rejection Reason
+        {{ rejectionReason }}
       </v-card-text>
     </v-card>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import keystore from '@/lib/keystore'
+import { LOG_REJECTED } from '@/constants/log-type'
+
 export default {
-  name: 'RejectAlert'
+  name: 'RejectAlert',
+  props: {
+    specimen: Object,
+  },
+  computed: {
+    ...mapState({
+      logContract: state => state.ethereum.contracts.contractDegenicsLog,
+    }),
+    rejectionReason() {
+      return this.logs.length > 0
+      ? this.logs.filter(l => l.logType == LOG_REJECTED)[0].log
+      : ''
+    },
+  },
+  data: () => ({
+    LOG_REJECTED
+  }),
+  mounted() {
+    this.getLogs() 
+  },
+  methods: {
+    async getLogs() {
+      const ks = keystore.get()
+      const logCount = await this.logContract.methods
+        .countSpecimenLog(this.specimen.number).call({ from: ks.address })
+      
+      const logPromises = []
+      for (let i = 1; i <= logCount; i++) {
+        const promise = this.logContract.methods
+          .specimenLogByIndex(this.specimen.number, i).call()
+        logPromises.push(promise)
+      }
+      const logs = await Promise.all(logPromises)
+
+      this.logs = logs
+    },
+  }
 }
 </script>
 
