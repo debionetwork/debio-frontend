7b0673f4d61376cdee0b7392bdca54b672d8982f
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-04-10 11:41:15 +0700

    feat: removed bip39 from library and upgraded @polkadot/api.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
 const WorkerPlugin = require('worker-plugin')
 
 module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('mjs$')
+      .test(/\.mjs$/)
+      .include
+      .add(/node_modules/)
+      .end()
+      .type('javascript/auto');
+  },
+  configureWebpack: {
+    resolve: {
+      extensions: ['*', '.mjs', '.js', '.vue', '.json']
+    }
+  },
   "transpileDependencies": [
     "vuetify",
     "eslint-loader",
