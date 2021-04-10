9a8a4961fd5a1b57f3139518b29f28e68c87957c
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-04-10 11:35:33 +0700

    refactor: moved file locations on lib folder.

--- src/lib/substrate/index.js
+++ src/lib/substrate/index.js
@@ -1,13 +1,13 @@
-import { ApiPromise, WsProvider } from '@polkadot/api'
-export { default as Wallet } from './Wallet'
+// import { ApiPromise, WsProvider } from '@polkadot/api'
+export { default as Wallet } from './wallet'
 
 export async function connect() {
   try {
-    const PROVIDER_SOCKET = 'ws://127.0.0.1:9944'
-    const wsProvider = new WsProvider(PROVIDER_SOCKET)
-    const api = await ApiPromise.create({ provider: wsProvider })
-    await api.isReady
-    return api
+    // const PROVIDER_SOCKET = 'wss://debio.theapps.dev/node'
+    // const wsProvider = new WsProvider(PROVIDER_SOCKET)
+    // const api = await ApiPromise.create({ provider: wsProvider })
+    // await api.isReady
+    // return api
 
   } catch (err) {
     throw new Error(err)
