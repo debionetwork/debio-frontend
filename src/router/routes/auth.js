b349caea386db10e2695afd14c2fe879fc4eb2ee
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-04-10 11:40:08 +0700

    refactor: separated routes for different contexts.

---
+++ src/router/routes/auth.js
@@ -0,0 +1,13 @@
+const authRoutes = [
+  {
+    path: '/login',
+    name: 'login',
+    meta: { public: true },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "" */ '../../views/Login')
+  },
+]
+
+export default authRoutes
