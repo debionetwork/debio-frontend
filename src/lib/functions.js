46dcb285de1eb3455195b975ed4b443606164a67
Aullia Rachman <auelvie@gmail.com>
2021-04-21 13:15:42 +0700

    fix: fix bug checking route request

--- src/lib/functions.js
+++ src/lib/functions.js
@@ -9,18 +9,18 @@ const globalFunctions = {
   async checkIsLoggedIn() {
     const isLoggedIn = this.hasStatusLogin();
     const address = localStorage.getAddress();
-    if (!isLoggedIn && router.currentRoute.fullPath != '/login') {
+    if (!isLoggedIn && window.location.pathname != '/login') {
       router.push('/login')
       return ""
     }
 
-    if (isLoggedIn && router.currentRoute.fullPath == '/login') {
+    if (isLoggedIn && window.location.pathname == '/login') {
       router.push('/')
       return address;
     }
 
     // is logged in, continue
-    router.push(router.currentRoute.fullPath)
+    router.push(window.location.pathname)
     return address
   }
 }
