fcf10f67c2d295424b7803ee35e4271b13817ad8
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-05-03 02:08:18 +0700

    refactor: removed unused domain logic for substrate queries and used toHuman() instead.

--- src/lib/polkadotProvider/command/services.js
+++ src/lib/polkadotProvider/command/services.js
@@ -1,17 +1,20 @@
 export async function createService(api, pair, data){
-    return await api.tx.services
+    const result = await api.tx.services
         .createService(data)
         .signAndSend(pair)
+    return result.toHuman()
 }
 
 export async function updateService(api, pair, data){
-    return await api.tx.services
+    const result = await api.tx.services
         .updateService(data)
         .signAndSend(pair)
+    return result.toHuman()
 }
 
 export async function deleteService(api, pair, data){
-    return await api.tx.services
+    const result = await api.tx.services
         .deleteService(data)
         .signAndSend(pair)
+    return result.toHuman()
 }
--- src/lib/polkadotProvider/query/geneticTesting.js
+++ src/lib/polkadotProvider/query/geneticTesting.js
@@ -1,30 +1,16 @@
-import { Keyring } from '@polkadot/keyring'
-
 export async function queryDnaSamples(api, bytes){
     const res = await api.query.geneticTesting.dnaSamples(bytes)
     return res.toHuman()
 }
 
 export async function queryDnaSamplesByLab(api, labId){
-    const keyring = new Keyring()
-    const res = (await api.query.geneticTesting.dnaSamplesByLab(labId)).value
-
-    let dnaSampleTrackingIds = []
-    for(let i = 0; i < res.length; i++){
-        dnaSampleTrackingIds.push(keyring.encodeAddress(res[i], 42))
-    }
-    return dnaSampleTrackingIds
+    const res = await api.query.geneticTesting.dnaSamplesByLab(labId)
+    return res.toHuman()
 }
 
 export async function queryDnaSamplesByOwner(api, ownerId){
-    const keyring = new Keyring()
-    const res = (await api.query.geneticTesting.dnaSamplesByOwner(ownerId)).value
-
-    let dnaSampleTrackingIds = []
-    for(let i = 0; i < res.length; i++){
-        dnaSampleTrackingIds.push(keyring.encodeAddress(res[i], 42))
-    }
-    return dnaSampleTrackingIds
+    const res = await api.query.geneticTesting.dnaSamplesByOwner(ownerId)
+    return res.toHuman()
 }
 
 export async function queryDnaTestResults(api, bytes){
@@ -33,23 +19,11 @@ export async function queryDnaTestResults(api, bytes){
 }
 
 export async function queryDnaTestResultsByLab(api, labId){
-    const keyring = new Keyring()
-    const res = (await api.query.geneticTesting.dnaTestResultsByLab(labId)).value
-
-    let dnaSampleTrackingIds = []
-    for(let i = 0; i < res.length; i++){
-        dnaSampleTrackingIds.push(keyring.encodeAddress(res[i], 42))
-    }
-    return dnaSampleTrackingIds
+    const res = await api.query.geneticTesting.dnaTestResultsByLab(labId)
+    return res.toHuman()
 }
 
 export async function queryDnaTestResultsByOwner(api, ownerId){
-    const keyring = new Keyring()
-    const res = (await api.query.geneticTesting.dnaTestResultsByOwner(ownerId)).value
-
-    let dnaSampleTrackingIds = []
-    for(let i = 0; i < res.length; i++){
-        dnaSampleTrackingIds.push(keyring.encodeAddress(res[i], 42))
-    }
-    return dnaSampleTrackingIds
+    const res = await api.query.geneticTesting.dnaTestResultsByOwner(ownerId)
+    return res.toHuman()
 }
