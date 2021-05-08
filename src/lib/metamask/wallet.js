16e88d7a5d08a9ee134a80957e19ca2ef481d05c
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-05-09 05:27:43 +0700

    feat: added new sendTransaction function for metamask wallet.

--- src/lib/metamask/wallet.js
+++ src/lib/metamask/wallet.js
@@ -1,15 +1,67 @@
-import Web3 from 'web3'
 import abi from './usdt/abi.json'
 import { usdtAddress } from './usdt'
+import store from '../../store'
 
 export async function getBalance(address) {
-  const web3 = new Web3('https://mainnet.infura.io/v3/undefined')
+  const web3 = store.getters['ethereum/getWeb3']
   const tokenContract = new web3.eth.Contract(abi, usdtAddress())
   const balance = await tokenContract.methods
     .balanceOf(address)
     .call()
   return balance
 }
+
+export function sendTransaction(from, to, value) {
+  const web3 = store.getters['ethereum/getWeb3']
+  const tokenContract = new web3.eth.Contract(abi, usdtAddress())
+  return await tokenContract.methods.sendTransaction({
+    from: from,
+    to: to,
+    value: value
+  })
+}
+
+/**
+ * sendTransaction
+ *
+ * @param {String} contractAddress -> the contractAddress to send transaction to
+ * @param {Wallet} senderAddress -> the transaction sender's ethereum-js Wallet object
+ * @param {String} data -> The encoded ABI byte code to send via a transaction or call.
+ *                          Example: myContract.methods.myMethod(123).encodeABI();
+ * @param {String} value -> amount of ether to transfer
+ * */
+//  async function sendTransaction(contractAddress, senderWallet, data, value) {
+//   const web3 = store.getters['ethereum/getWeb3']
+
+//   let nonce = await web3.eth.getTransactionCount(senderWallet.getAddressString())
+
+//   // Create transaction
+//   const rawTx = {
+//     nonce: web3.utils.toHex(nonce),
+//     gasLimit: web3.utils.toHex(appConfig.getGasLimit()),
+//     gasPrice: web3.utils.toHex(appConfig.getGasPrice()),
+//     from: senderWallet.getAddressString(),
+//     to: contractAddress,
+//     data
+//   }
+
+//   if (value) {
+//     rawTx.value = web3.utils.toHex(value)
+//   }
+//   const tx = new Transaction(rawTx)
+
+//   // Sign transaction
+//   const privateKeyBuffer = EthUtil.toBuffer(senderWallet.getPrivateKeyString())
+//   tx.sign(privateKeyBuffer)
+
+//   // Serialize transaction
+//   const serializedTx = tx.serialize().toString('hex')
+
+//   // Send transaction
+//   const txReceipt = await web3.eth.sendSignedTransaction('0x' + serializedTx)
+
+//   return txReceipt
+// }
 
 export async function changeChain(){
     await window.ethereum.request({
