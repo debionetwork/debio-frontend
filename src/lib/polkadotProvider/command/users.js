88f039c00b3274842194512b1423776f5015e15e
Aullia Rachman <auelvie@gmail.com>
2021-05-17 00:00:43 +0700

    feat: update method pallet userProfile and rpc address

--- src/lib/polkadotProvider/command/users.js
+++
@@ -1,7 +0,0 @@
-// set EthereumAddress to pallet
-export async function setUsdtAddress(api, pair, usdt_address) {
-  const result = await api.tx.userProfile
-    .setUsdtAddress(usdt_address)
-    .signAndSend(pair)
-  return result.toHuman()
-}
--- src/lib/polkadotProvider/query/userProfile.js
+++ src/lib/polkadotProvider/query/userProfile.js
@@ -1,4 +1,9 @@
-export async function usdtAddresses(api, address) {
-    const res = await api.query.userProfile.usdtAddresses(address)
+export async function ethAddressByAccountId(api, address) {
+    const res = await api.query.userProfile.ethAddressByAccountId(address)
+    return res.toHuman()
+}
+
+export async function accountIdByEthAddress(api, ethAddress) {
+    const res = await api.query.userProfile.accountIdByEthAddress(ethAddress)
     return res.toHuman()
 }
