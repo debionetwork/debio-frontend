cec0e1f519c868cab69b03ed6fe41b3f71818e7f
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-06-04 05:55:10 +0700

    feat: added EMR provider.

---
+++ src/lib/polkadotProvider/command/electronicMedicalRecord.js
@@ -0,0 +1,27 @@
+export async function registerElectronicMedicalRecord(api, pair) {
+    const result = await api.tx.electronicMedicalRecord
+        .addElectronicMedicalRecord()
+        .signAndSend(pair, { nonce: -1 })
+    return result.toHuman()
+}
+
+export async function deregisterElectronicMedicalRecord(api, pair) {
+    const result = await api.tx.electronicMedicalRecord
+        .removeElectronicMedicalRecord()
+        .signAndSend(pair, { nonce: -1 })
+    return result.toHuman()
+}
+
+export async function addElectronicMedicalRecordInfo(api, pair, data) {
+    const result = await api.tx.electronicMedicalRecord
+        .addElectronicMedicalRecordInfo(data)
+        .signAndSend(pair, { nonce: -1 })
+    return result.toHuman()
+}
+
+export async function removeElectronicMedicalRecordInfo(api, pair) {
+    const result = await api.tx.electronicMedicalRecord
+        .removeElectronicMedicalRecordInfo()
+        .signAndSend(pair, { nonce: -1 })
+    return result.toHuman()
+}
