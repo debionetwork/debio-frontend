73da134d0c68c1ebf6ca5841daa3758d399191f4
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-06-15 16:58:22 +0700

    fix: changed button text

--- src/lib/polkadotProvider/query/orders.js
+++ src/lib/polkadotProvider/query/orders.js
@@ -1,6 +1,5 @@
 import { queryServicesById } from './services'
 import { queryLabsById } from './labs'
-import { queryDnaSamples } from './geneticTesting'
 
 export async function ordersByCustomer(api, address) {
   const res = await api.query.orders.ordersByCustomer(address)
@@ -13,12 +12,7 @@ export async function getOrdersDetailByAddress(api, address) {
   for(let i = 0; i < orderIds.length; i++){
     let orderDetail = await getOrdersDetail(api, orderIds[i])
     const service = await queryServicesById(api, orderDetail.service_id)
-    const dnaSample = await queryDnaSamples(api, orderDetail.dna_sample_tracking_id)
     orderDetail['created_at'] = parseInt(orderDetail.created_at.replace(/,/g, ""))
-    
-    if(dnaSample != null){
-      orderDetail['dna_sample_status'] = dnaSample.status
-    }
     
     let lab = null
     if(service != null){
