8b0b14fc0de3a8579d3fd643aaf6dd770f626d8e
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-07-02 16:02:15 +0700

    feat: added server side pagination datatable on order history.

---
+++ src/components/DataTable/ServerSideDataTable/NumOfEntriesSelect.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="d-flex align-center justify-end">
+    <div> Showing </div>
+    <div class="px-2 num-of-entries-select">
+      <v-select
+        hide-details="auto"
+        :items="numOfEntriesSelect"
+        v-model="numOfEntries"
+        color="secondary"
+        outlined
+        dense
+        @change="change"
+      ></v-select>
+    </div>
+    <div>entries</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: [Number, String],
+    change: Function,
+  },
+  data: () => ({
+    numOfEntriesSelect: [5, 10, 15, 20]
+  }),
+  computed: {
+    numOfEntries: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('select', val)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/variables.scss';
+
+.num-of-entries-select {
+  /** Border color */
+  fieldset {
+    border: 2px solid $color-border;
+  }
+  /** border color when focused */
+  .v-input--is-focused {
+    fieldset {
+      border: 2px solid $color-secondary !important;
+    }
+  }
+
+  .v-input {
+    width: 70px; 
+  }
+  .v-input__slot {
+    background: $color-form-background !important;
+    box-shadow: none !important;
+    height: 48px;
+
+    .v-select__selections {
+      overflow: visible;
+    }
+  }
+  /** outermost icon container */
+  .v-input__append-inner {
+    width: 20px;
+    margin-top: 12px !important;
+  }
+}
+</style>
---
+++ src/components/DataTable/ServerSideDataTable/index.vue
@@ -0,0 +1,226 @@
+<template>
+  <v-card class="degenics-datatable-card">
+    <slot name="prepend"></slot>
+    <v-row>
+      <!-- Search Bar -->
+      <v-col cols="12" sm="8" md="9" class="pt-0">
+        <slot name="search-bar"></slot>
+      </v-col>
+
+      <!-- Number of Entries Select -->
+      <v-col cols="12" sm="4" md="3" class="pt-0">
+        <NumOfEntriesSelect
+          :value="entriesPerPage"
+          :change="handlePageSizeChange"
+          @select="setEntriesPerPage"
+        >
+        </NumOfEntriesSelect>
+      </v-col>
+    </v-row>
+
+    <!-- Data Table -->
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :search="dataTableSearchVal"
+      :items-per-page="entriesPerPage"
+      :page.sync="page"
+      class="degenics-data-table"
+      :class="additionalClass"
+      hide-default-footer
+      :custom-filter="customFilter ? customFilter : defaultFilter"
+      :sort-by="sortBy"
+      :sort-desc="sortDesc"
+      :loading="loading"
+      :loading-text="computedLoadingText"
+    >
+      <!-- Slots for row formatting -->
+      <template v-for="(slot, name) in $scopedSlots" v-slot:[name]="item">
+        <slot :name="name" v-bind="item"></slot>
+      </template>
+
+      <!-- Custom Footer -->
+      <template v-slot:footer>
+        <div class="footer d-flex justify-space-between align-center flex-wrap">
+          <div class="pagination-info">
+            Showing {{ from() }} to {{ to() }} of {{ total() }} entries
+          </div>
+          <v-pagination
+            v-model="page"
+            :length="pageCount"
+            :total-visible="10"
+            circle
+            color="highlight"
+            @input="handlePageChange"
+          ></v-pagination>
+        </div>
+      </template>
+    </v-data-table>
+
+  </v-card>
+</template>
+
+<script>
+import NumOfEntriesSelect from './NumOfEntriesSelect'
+
+export default {
+  name: 'ServerSideDataTable',
+  components: {
+    NumOfEntriesSelect,
+  },
+  props: {
+    headers: Array,
+    items: Array,
+    totalItemLength: Number,
+    searchLabel: String,
+    search: String,
+    additionalClass: String,
+    customFilter: Function,
+    handlePageChange: Function,
+    handlePageSizeChange: Function,
+    sortBy: Array,
+    sortDesc: Array,
+    loading: Boolean,
+    loadingText: String,
+    pageSize: Number,
+    currentPage: Number,
+  },
+  watch: {
+    currentPage: function(newVal, oldVal) { // watch it
+      if(newVal != oldVal) console.log('Change page!')
+      this.page = newVal
+    }
+  },
+  data: () => ({
+    page: 1,
+    entriesPerPage: 10
+  }),
+  mounted(){
+    this.page = this.currentPage
+    this.entriesPerPage = this.pageSize
+  },
+  computed: {
+    dataTableSearchVal: {
+      get() {
+        return this.search
+      },
+      set(val) {
+        console.log(val)
+        this.$emit('input', val)
+      }
+    },
+    pageCount(){
+      return Math.ceil(this.totalItemLength / this.entriesPerPage)
+    },
+    computedLoadingText() {
+      return this.loadingText ? this.loadingText : 'Loading.. Please wait'
+    }
+  },
+  methods: {
+    setEntriesPerPage(val) {
+      this.entriesPerPage = val
+    },
+    defaultFilter(value, search, /*item*/) {
+      return value != null &&
+      search != null &&
+      typeof value !== 'boolean' &&
+      value.toString().toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) !== -1
+    },
+    /**
+     * Footer Helper functions
+     */
+    from() {
+      if (this.totalItemLength == 0) {
+        return 0
+      }
+      return this.entriesPerPage * (this.page - 1) + 1
+    },
+    to() {
+      if (this.page * this.entriesPerPage > this.totalItemLength) {
+        return this.totalItemLength
+      }
+      return this.page * this.entriesPerPage
+    },
+    total() {
+      return this.totalItemLength
+    },
+    /** ----------------- */
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/variables.scss';
+
+.degenics-datatable-card {
+  border-radius: 8px !important;
+  overflow: hidden !important;
+  padding: 40px;
+
+  box-shadow: unset !important;
+  filter: drop-shadow(0px 7px 20px rgba(0, 0, 0, 0.07));
+}
+
+.degenics-data-table {
+  border-radius: 8px;
+  overflow: hidden;
+  margin-top: 24px;
+
+  /** BRI text-body */
+  font-size: 14px;
+  line-height: 18px;
+  letter-spacing: 0.1px;
+  /** */
+
+  thead {
+    background-color: $color-secondary !important;
+
+    th {
+      padding-top: 8px !important;
+      padding-bottom: 8px !important;
+      /* white-space: nowrap; */
+      * {
+        color: white !important;
+        /** BRI text-body */
+        font-size: 14px;
+        line-height: 18px;
+        letter-spacing: 0.1px;
+        /** */
+      }
+      i.v-icon.notranslate.v-data-table-header__icon:not(:hover) {
+        opacity: 0.5;
+      }
+      &.active {
+        i.v-icon.notranslate.v-data-table-header__icon {
+          opacity: 1;
+        }
+      }
+    }
+  }
+  .footer {
+    height: 72px;
+
+    .pagination-info {
+      color: $color-text-secondary;
+    }
+    .v-pagination {
+      button {
+        font-size: 11px;
+        min-width: 24px;
+        height: 24px;
+        outline: none;
+        /* outline-color: $color-secondary; */
+        &:not(.v-pagination__item--active) {
+          background: $color-border;
+        }
+      }
+      li:first-child, li:last-child {
+        button {
+          width: 24px;
+        }
+      }
+    }
+  }
+}
+</style>
+
