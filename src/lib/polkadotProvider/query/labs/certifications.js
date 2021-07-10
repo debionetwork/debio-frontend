a65f15bdad2877980026cae9cb2780476425e4fc
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-07-10 21:30:56 +0700

    fix: certification not yet added (DBIO-240)

--- src/lib/polkadotProvider/query/labs/certifications.js
+++ src/lib/polkadotProvider/query/labs/certifications.js
@@ -1,11 +1,5 @@
-export async function getCertificationDetails(api, certificationIds) {
-  const certs = []
-  for (let i = 0; i < certificationIds.length; i++) {
-    const res = await api.query.certifications.certifications(certificationIds[i])
-    if (res.isSome) {
-      certs.push(res.toHuman())
-    }
-  }
 
-  return certs
+export async function getCertificationDetail(api, certificationId){
+  const res = await api.query.certifications.certifications(certificationId)
+  return res.toHuman()
 }
