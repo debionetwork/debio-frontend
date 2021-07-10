8c9bad013cad9b5bf1c3d5cbe318ec1ddcd6eb0a
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-07-11 00:28:03 +0700

    feat: added certification feature on hospital page and fix hospital registration and account form.

--- src/lib/polkadotProvider/query/hospitals/certifications.js
+++ src/lib/polkadotProvider/query/hospitals/certifications.js
@@ -1,12 +1,4 @@
-export async function getHospitalCertificationDetails(api, hospitalCertificationIds) {
-    const certs = []
-    for (let i = 0; i < hospitalCertificationIds.length; i++) {
-      const res = await api.query.hospitalCertifications.hospitalCertifications(hospitalCertificationIds[i])
-      if (res.isSome) {
-        certs.push(res.toHuman())
-      }
-    }
-  
-    return certs
-  }
-  
+export async function getHospitalCertificationDetail(api, hospitalCertificationId){
+  const res = await api.query.hospitalCertifications.hospitalCertifications(hospitalCertificationId)
+  return res.toHuman()
+}
