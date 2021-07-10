ad65a6ef3d1e3a23aaf69525c36d7235a9d3d094
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-07-10 23:50:44 +0700

    feat: added certification feature on doctor page (DBIO-295)

--- src/lib/polkadotProvider/query/doctors/certifications.js
+++ src/lib/polkadotProvider/query/doctors/certifications.js
@@ -1,12 +1,4 @@
-export async function getDoctorCertificationDetails(api, doctorCertificationIds) {
-    const certs = []
-    for (let i = 0; i < doctorCertificationIds.length; i++) {
-      const res = await api.query.doctorCertifications.doctorCertifications(doctorCertificationIds[i])
-      if (res.isSome) {
-        certs.push(res.toHuman())
-      }
-    }
-  
-    return certs
-  }
-  
+export async function getDoctorCertificationDetail(api, doctorCertificationId){
+  const res = await api.query.doctorCertifications.doctorCertifications(doctorCertificationId)
+  return res.toHuman()
+}
