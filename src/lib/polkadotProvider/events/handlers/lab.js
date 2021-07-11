7b914015adf1486709be5c8673414dc642245f01
auelvie@gmail.com <auelvie@gmail.com>
2021-07-11 22:53:42 +0700

    fix: fix notifcation balance transfer and balances for lab and customer.

--- src/lib/polkadotProvider/events/handlers/lab.js
+++ src/lib/polkadotProvider/events/handlers/lab.js
@@ -1,37 +1,40 @@
+import { toFormatDebioCoin } from "@/lib/polkadotProvider/index"
+
 const defaultHandler = {
-    get: function(target, name){
-        return Object.prototype.hasOwnProperty.call(target, name) ? target[name] : (dataEvent, value, valueMessage) => {
-            const data = dataEvent[0];
-            const id = data[value];
-            const params = { number: id };
-            const wording = "for (" + data[valueMessage].substr(0, 4) + "..." + data[valueMessage].substr(data[valueMessage].length - 4) + ")";
-            return { data, id, params, wording }
-        }
-    }
+	get: function (target, name) {
+		return Object.prototype.hasOwnProperty.call(target, name) ? target[name] : (dataEvent, value, valueMessage) => {
+			const data = dataEvent[0];
+			const id = data[value];
+			const params = { number: id };
+			const wording = "for (" + data[valueMessage].substr(0, 4) + "..." + data[valueMessage].substr(data[valueMessage].length - 4) + ")";
+			return { data, id, params, wording }
+		}
+	}
 }
 
 const handler = {
-    balances: (dataEvent, value, valueMessage) => {
-        const data = dataEvent;
-        const id = data[value];
-        const params = { number: id };
-        const wording = data[valueMessage] + " DBIO!";
-        return { data, id, params, wording }
-    },
-    orders: (dataEvent, value, valueMessage) => {
-        const data = dataEvent[0];
-        const id = data[value];
-        const params = data;
-        const wording = "for (" + data[valueMessage].substr(0, 4) + "..." + data[valueMessage].substr(data[valueMessage].length - 4) + ")";
-        return { data, id, params, wording }
-    },
-    geneticTesting: (dataEvent, value, valueMessage) => {
-        const data = dataEvent[0];
-        const id = data[value];
-        const params = { number: id };
-        const wording = "for (" + data[valueMessage].substr(0, 4) + "..." + data[valueMessage].substr(data[valueMessage].length - 4) + ")";
-        return { data, id, params, wording }
-    },
+	balances: async (dataEvent, value, valueMessage) => {
+		const data = dataEvent;
+		const id = data[value];
+		const params = { number: id };
+		const finalText = await toFormatDebioCoin(data[valueMessage])
+		const wording = finalText + " DBIO!"
+		return { data, id, params, wording }
+	},
+	orders: async (dataEvent, value, valueMessage) => {
+		const data = dataEvent[0];
+		const id = data[value];
+		const params = data;
+		const wording = "for (" + data[valueMessage].substr(0, 4) + "..." + data[valueMessage].substr(data[valueMessage].length - 4) + ")";
+		return { data, id, params, wording }
+	},
+	geneticTesting: async (dataEvent, value, valueMessage) => {
+		const data = dataEvent[0];
+		const id = data[value];
+		const params = { number: id };
+		const wording = "for (" + data[valueMessage].substr(0, 4) + "..." + data[valueMessage].substr(data[valueMessage].length - 4) + ")";
+		return { data, id, params, wording }
+	},
 }
 
 // If property not found, return defaultHandler
