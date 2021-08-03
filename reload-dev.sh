5dce8abc81a2209ed9447657d107077090beae23
Kevin <kevin.janada@gmail.com>
2021-08-03 11:55:22 +0700

    ci: Fix wrong port exposed in reload-dev.sh script
    - Change exposed port from 8000 -> 8080

--- reload-dev.sh
+++ reload-dev.sh
@@ -4,4 +4,4 @@ docker stop debio_frontend_dev || true && docker rm debio_frontend_dev || true
 docker rmi asia.gcr.io/degenics/debio_frontend_dev:latest
 docker pull asia.gcr.io/degenics/debio_frontend_dev:latest
 echo "reload container .............."
-docker run -d --name=debio_frontend_dev --env=debioenv=development -v /etc/localtime:/etc/localtime:ro -p 0.0.0.0:8000:80 --restart=always asia.gcr.io/degenics/debio_frontend_dev:latest
+docker run -d --name=debio_frontend_dev --env=debioenv=development -v /etc/localtime:/etc/localtime:ro -p 0.0.0.0:8080:80 --restart=always asia.gcr.io/degenics/debio_frontend_dev:latest
