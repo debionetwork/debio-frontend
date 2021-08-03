e133ed451037a798bc9cca90f514be39953aff09
Agustinus Theodorus <37565691+agustinustheo@users.noreply.github.com>
2021-08-03 09:16:19 +0800

    feat: updated the services query (DBIO-402). (#110)
    * feat: added new profile_image field on lab registration page.
    
    * feat: updated the services query (DBIO-402).
    
    Co-authored-by: Agustinus Theodorus <agustinus.theodorus@binus.edu>

--- src/views/Dashboard/Lab/Services/index.vue
+++ src/views/Dashboard/Lab/Services/index.vue
@@ -38,7 +38,15 @@
                   <template v-slot:[`item.info.image`]="{ item }">
                      <v-container rounded>
                         <v-img 
-                           :src="getImageLink(item.info.image)" 
+                           v-if="item.info.image"
+                           :src="item.info.image" 
+                           :alt="item.name" 
+                           max-height="70px"
+                           max-width="70px"
+                        ></v-img>
+                        <v-img 
+                           v-else
+                           src="@/assets/debio-logo.png" 
                            :alt="item.name" 
                            max-height="70px"
                            max-width="70px"
@@ -109,12 +117,6 @@ export default {
    },
    gotoDetails(item){
       this.$router.push({ name: 'lab-dashboard-services-detail', params: { item: item }})
-   },
-   getImageLink(val){
-      if(val && val != ""){
-         return val
-      }
-      return "https://ipfs.io/ipfs/QmaGr6N6vdcS13xBUT4hK8mr7uxCJc7k65Hp9tyTkvxfEr"
    },
   },
 }
