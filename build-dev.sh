b1c0bc23426f4d8ca2a0a752a0d0c17c555f2bf3
Kevin <kevin.janada@gmail.com>
2021-08-10 10:08:11 +0700

    ci: Change docker registry in deploy scripts

--- build-dev.sh
+++ build-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t debio_frontend_dev .
-docker tag debio_frontend_dev asia.gcr.io/degenics/debio_frontend_dev:latest
-docker push asia.gcr.io/degenics/debio_frontend_dev:latest
+docker tag debio_frontend_dev hub.debio.network/debio_frontend_dev:latest
+docker push hub.debio.network/debio_frontend_dev:latest
