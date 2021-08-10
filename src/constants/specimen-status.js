02c35b2c0c87ec76f4f93d6741824b63765fe409
bukandicki <bukandicki@gmail.com>
2021-08-10 22:09:23 +0700

    chore(Order History): Add new failed page

--- src/constants/specimen-status.js
+++ src/constants/specimen-status.js
@@ -6,9 +6,9 @@ export const PROCESS = 'Processing'
 export const PROCESSED = 'Processed'
 export const FAILED = 'Failed'
 
-
 export const ORDER_UNPAID = 'Unpaid'
 export const ORDER_PAID = 'Paid'
 export const ORDER_FULFILLED = 'Success'
 export const ORDER_REFUNDED = 'Refunded'
 export const ORDER_CANCEL = 'Cancelled'
+export const ORDER_FAILED = 'Failed'
