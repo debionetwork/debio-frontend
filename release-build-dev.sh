84a91bef6f50f4cddafece8dca099c409054a6a8
M Azwar Nurrosat <azwar.nurrosat@tokopedia.com>
2021-08-31 08:28:44 +0700

    test build image

--- release-build-dev.sh
+++ release-build-dev.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-docker build -f release.dockerfile -t hub.debio.network/debio_frontend_dev:latest .
+docker build -f ./release.dockerfile -t debio_frontend_dev .
+docker tag debio_frontend_dev hub.debio.network/debio_frontend_dev:latest
 docker push hub.debio.network/debio_frontend_dev:latest
