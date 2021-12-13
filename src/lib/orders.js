369c7f70e7ab1f41a1374d91bfa4a3416ab3dde9
Dicki <bukandicki@gmail.com>
2021-12-13 09:35:45 +0700

    Update ENV

--- src/lib/orders.js
+++ src/lib/orders.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const getOrdersData = async (hash, page, size, keyword = "") => {
   const { data } = await axios.get(
-    `${process.env.VUE_APP_DEV_DEBIO_BACKEND_URL}/orders/list/lab/${hash}`, {
+    `${process.env.VUE_APP_BACKEND_API}/orders/list/lab/${hash}`, {
       params: {
         keyword,
         size,
