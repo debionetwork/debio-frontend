27aec154812b6b9f2d3381ccd36428f0c356336c
Mezi Aris Apronny <60445461+meziaris@users.noreply.github.com>
2021-12-23 17:56:15 +0700

    ci: enable autoscaling (#320)
    * ci: use gcr private repository
    
    * ci: fix image tag
    
    * ci: enable autoscaling
    
    * ci: remove unused files
    
    * ci: add new env

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker build -t debio_frontend_service .
-docker tag debio_frontend_service hub.theapps.dev/debio_frontend_service:latest
-docker push hub.theapps.dev/debio_frontend_service:latest
