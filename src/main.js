be96303f399f699a0d584d72ba1b543383d8dccd
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-01-03 12:47:40 +0700

    feat: sentry analytics.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import router from './router'
-import store from './store'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import { fmtSpecimenNumber } from './lib/string-format'
-import { format, fromUnixTime } from 'date-fns'
+import Vue from "vue"
+import router from "./router"
+import store from "./store"
+import App from "./App.vue"
+import vuetify from "./plugins/vuetify";
+import { fmtSpecimenNumber } from "./lib/string-format"
+import { format, fromUnixTime } from "date-fns"
 import VueGtag from "vue-gtag"
+import * as Sentry from "@sentry/vue"
+import { Integrations } from "@sentry/tracing"
 
-var pjson = require('./package.json')
+var pjson = require("../package.json")
 
 Vue.use(VueGtag, {
   config: { id: process.env.VUE_APP_GTAG },
@@ -15,34 +17,50 @@ Vue.use(VueGtag, {
   pageTrackerScreenviewEnabled: true
 }, router)
 
+const SENTRY_DSN = process.env.VUE_APP_SENTRY_DSN
+
+if (SENTRY_DSN) {
+  Sentry.init({
+    Vue,
+    dsn: SENTRY_DSN,
+    integrations: [
+      new Integrations.BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+        tracingOrigins: ["localhost", "app.debio.network", /^\//],
+      }),
+    ],
+    tracesSampleRate: 1.0,
+  })
+}
+
 Vue.config.productionTip = false
 
-Vue.filter('customerSpecimenStatus', function (val) {
-  return val == 'Received' ? 'Received by Lab' : val
+Vue.filter("customerSpecimenStatus", function (val) {
+  return val == "Received" ? "Received by Lab" : val
 })
-Vue.filter('specimenNumber', function (val) {
+Vue.filter("specimenNumber", function (val) {
   return fmtSpecimenNumber(val)
 })
-Vue.filter('timestampToDate', function (val) {
+Vue.filter("timestampToDate", function (val) {
   let timestamp
   if (typeof val == "string") {
-    timestamp = parseInt(val.replaceAll(',', ''))
+    timestamp = parseInt(val.replaceAll(",", ""))
   } else {
     timestamp = val
   }
   try {
-    return format(new Date(timestamp), 'MMMM dd yyyy')
+    return format(new Date(timestamp), "MMMM dd yyyy")
   } catch (err) {
     console.log(err)
-    return ''
+    return ""
   }
 })
-Vue.filter('timestampToDateTime', function (val) {
+Vue.filter("timestampToDateTime", function (val) {
   try {
-    return format(fromUnixTime(val), 'MMMM dd yyyy hh:mm')
+    return format(fromUnixTime(val), "MMMM dd yyyy hh:mm")
   } catch (err) {
     console.log(err)
-    return ''
+    return ""
   }
 })
 
@@ -63,9 +81,9 @@ async function setupAppDependencies() {
 */
 
 async function setupAppDependencies() {
-  await store.dispatch('auth/initApp');
-  await store.dispatch('metamask/initWeb3');
-  store.dispatch('metamask/contracts/initContracts');
+  await store.dispatch("auth/initApp");
+  await store.dispatch("metamask/initWeb3");
+  store.dispatch("metamask/contracts/initContracts");
   return
 }
 
@@ -79,6 +97,6 @@ setupAppDependencies()
       router,
       store,
       render: h => h(App)
-    }).$mount('#app')
+    }).$mount("#app")
 
   })
