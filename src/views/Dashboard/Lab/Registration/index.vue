f65a845a1ef842fd7ad6bbf30ca0fd6a1e9cdb3f
Dicki Maulana <bukandicki@gmail.com>
2022-01-24 14:56:03 +0700

    remove duplicate class

--- src/views/Dashboard/Lab/Registration/index.vue
+++ src/views/Dashboard/Lab/Registration/index.vue
@@ -246,7 +246,7 @@ export default {
 
     websiteRules() { 
       return [
-        val => /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/.test(val) || 'Website is invalid. It should contain protocol (https://) followed by a domain'
+        val => /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&/=]*)/.test(val) || 'Website is invalid. It should contain protocol (https://) followed by a domain'
       ]
     },
 
