b87353a9806f91c7126d0274119bcadad796676c
Azmi Muhammad <azmim85@gmail.com>
2022-04-05 16:06:46 +0700

    fix: enhance lab notification

--- src/lib/polkadotProvider/events/index.js
+++ src/lib/polkadotProvider/events/index.js
@@ -6,33 +6,33 @@ const handlers = {
   lab: labHandler
 }
 
-export async function processEvent(state, address, event, role){
+export async function processEvent(state, address, event, role) {
   let statusAdd = false
   let message = ""
   let data = null
   let params = null
-    
+
   const dataEvent = JSON.parse(event.data.toString())
   dataEvent.push(event.method)
 
   if (dataEvent.length > 0) {
     let handler = handlers[role][event.section]
-    if(!handler){
+    if (!handler) {
       console.log("no role mapping")
       return { statusAdd, message, data, params }
     }
-        
+
     // Get event configuration data
     const value = state.configEvent["role"][role][event.section][event.method].value
     const valueMessage = state.configEvent["role"][role][event.section][event.method].value_message
-    const identity = state.configEvent["role"][role][event.section][event.method].identity  
-        
+    const identity = state.configEvent["role"][role][event.section][event.method].identity
+
     const res = await handler(dataEvent, value, valueMessage)
 
     // Hardcode condition for LabUpdateVerificationStatus
-    if(
+    if (
       res.data[2] == "LabUpdateVerificationStatus" &&
-            res.data[0].accountId == address
+      res.data[0].accountId == address
     ) {
       statusAdd = true
       message = res.wording
