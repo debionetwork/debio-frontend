e5a0814441690e9160b34b403c67512884abe9de
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-04-13 04:09:03 +0700

    tests: lab dashboard unit tests

---
+++ tests/unit/views/Dashboard/Lab/OrderHistory/ProcessOrder/index.spec.js
@@ -0,0 +1,50 @@
+import { createLocalVue, shallowMount } from "@vue/test-utils"
+import ProcessOrder from "@/views/Dashboard/Lab/OrderHistory/ProcessOrder"
+import Vuex from "vuex"
+import Vuetify from "vuetify"
+import _ from "lodash"
+
+describe("Dashboard Lab Order History Process Order Index", () => {
+  let container
+  let localVue = null
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(Vuex)
+    localVue.use(Vuetify)
+  })
+
+  afterEach(() => {
+    localVue = null
+  })
+
+  it("Should render", () => {
+    // Arrange
+    const processOrder = _.cloneDeep(ProcessOrder)
+    processOrder.created = jest.fn()
+    processOrder.mounted = jest.fn()
+
+    container = shallowMount(processOrder, {
+      localVue,
+      vuetify: new Vuetify(),
+      store: new Vuex.Store({
+        state: {
+          substrate: {
+            api: "API",
+            wallet: "WALLET",
+            mnemonicData: "oil spend nation obey lecture behave lake diary reward forest gym apple"
+          },
+          auth: {
+            loadingData: "LOADING"
+          },
+          metamask: {
+            web3: "WEB3"
+          }
+        }
+      })
+    })
+
+    // Assert
+    expect(container.exists()).toBe(true)
+  })
+})
