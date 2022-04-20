35d69ee15f840ca7fb3367256bb5d73ecbe1443e
Azmi Muhammad <azmim85@gmail.com>
2022-04-20 15:15:46 +0700

    feat: enhance new wallet popup

--- src/components/HeaderUserInfo.vue
+++ src/components/HeaderUserInfo.vue
@@ -1,146 +1,35 @@
 <template>
-  <v-menu bottom left>
-    <template v-slot:activator="{ on, attrs }">
-      <div class="d-flex align-center mr-4" v-bind="attrs" v-on="on">
-        <div class="d-flex align-center">
-          <div style="cursor: pointer">
-            <v-img src="@/assets/user-profile-sample.png" max-width="35" />
-          </div>
-          <div>
-            <!-- :class="hover ? 'primary--text' : ''" -->
-            <div class="ml-3 text-left">
-              {{ name }}
-            </div>
-            <div class="ml-3 light_primary--text" style="font-size: 14px">
-              {{ balance }} DBIO
-            </div>
-          </div>
-        </div>
-      </div>
-    </template>
-    <div class="mt-11">
-      <v-card>
-        <v-list>
-          <v-list-item two-line @click="openWalletBinding">
-            <v-list-item-action>
-              <v-icon>mdi-wallet</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Wallet Binding</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item two-line @click="logOut">
-            <v-list-item-action>
-              <v-icon>mdi-logout</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Log Out</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-card>
+  <div class="d-flex align-center">
+    <div>
+      <v-img src="@/assets/user-profile-sample.png" max-width="35" />
     </div>
-  </v-menu>
+    <div>
+      <div class="ml-3 text-left">
+        {{ name }}
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
-import { mapState, mapMutations, mapActions } from "vuex";
-import localStorage from "@/lib/local-storage";
-import { ethAddressByAccountId } from "@/lib/polkadotProvider/query/userProfile";
-import { queryBalance } from "@/lib/polkadotProvider/query/balance";
-import { getBalanceETH } from "@/lib/metamask/wallet.js";
-import { fromEther } from "@/lib/balance-format"
+import { mapState } from "vuex";
 
 export default {
   name: "HeaderUserInfo",
+
   computed: {
     ...mapState({
-      walletBalance: (state) => state.substrate.walletBalance,
-      api: (state) => state.substrate.api,
-      wallet: (state) => state.substrate.wallet,
-      metamaskWalletAddress: (state) => state.metamask.metamaskWalletAddress,
-      metamaskWalletBalance: (state) => state.metamask.metamaskWalletBalance
+      wallet: (state) => state.substrate.wallet
     })
   },
+
   data: () => ({
-    isLoading: false,
-    balance: 0,
     name: ""
   }),
-  methods: {
-    ...mapMutations({
-      setWalletBalance: "substrate/SET_WALLET_BALANCE",
-      clearWallet: "substrate/CLEAR_WALLET",
-      setMetamaskAddress: "metamask/SET_WALLET_ADDRESS",
-      setMetamaskBalance: "metamask/SET_WALLET_BALANCE"
-    }),
-    ...mapActions({
-      clearAuth: "auth/clearAuth"
-    }),
-    async getBalance(balanceNummber) {
-      const balance = Number(await fromEther(balanceNummber)).toFixed(3)
-      this.balance = balance.replace(/\.000/,"")
-      if (this.balance == "0") {
-        this.balance = "0";
-      }
-    },
-    async fetchWalletBalance() {
-      try {
-        this.isLoading = true;
-        const balanceNummber = await queryBalance(
-          this.api,
-          this.wallet.address
-        );
-        this.getBalance(balanceNummber);
-        this.name = this.wallet.meta.name;
-        this.setWalletBalance(balanceNummber);
-        this.isLoading = false;
-      } catch (err) {
-        console.error(err);
-        this.isLoading = false;
-      }
-    },
-    logOut() {
-      this.clearAuth();
-      localStorage.clear();
-      this.$store.dispatch("lab/setProvideService", {})
-      this.$store.commit("substrate/SET_IS_SERVICES_EXIST", false)
-      this.$router.push("/login");
-    },
-    openWalletBinding() {
-      this.$emit("showWalletBinding", true);
-    },
-    async checkMetamaskAddress() {
-      if (this.metamaskWalletAddress == "") {
-        const ethRegisterAddress = await ethAddressByAccountId(
-          this.api,
-          this.wallet.address
-        );
-        if (ethRegisterAddress != null) {
-          this.setMetamaskAddress(ethRegisterAddress);
-        }
-      }
-    }
-  },
-  watch: {
-    async wallet() {
-      await this.fetchWalletBalance();
-    },
-    async metamaskWalletAddress() {
-      let balance = 0;
-      if (this.metamaskWalletAddress != "") {
-        balance = await getBalanceETH(this.metamaskWalletAddress);
-      }
-      this.setMetamaskBalance(balance);
-    },
-    walletBalance() {
-      this.getBalance(this.walletBalance);
-    }
-  },
+  
   async mounted() {
-    await this.fetchWalletBalance();
-    await this.checkMetamaskAddress();
+    this.name = this.wallet.meta.name;
   }
 };
 </script>
---
+++ src/components/HeaderWallet.vue
@@ -0,0 +1,291 @@
+<template>
+  <v-menu bottom left :close-on-content-click="false">
+    <template v-slot:activator="{on, attrs}">
+      <div class="d-flex align-center ml-10 mr-4" v-bind="attrs" v-on="on">
+        <div class="d-flex align-center">
+          <div style="cursor: pointer">
+            <v-icon>mdi-wallet</v-icon>
+          </div>
+        </div>
+      </div>
+    </template>
+    <div class="mt-11">
+      <v-card class="card-container">
+        <div>
+          <b class="notification-title">Wallet</b>
+          <div class="d-flex">
+            <div class="wallet-icon">
+              <v-img src="@/assets/polkadot-icon.svg" />
+            </div>
+            <b class="notification-subtitle">Polkadot</b>
+          </div>
+          <div class="text-content">Address</div>
+          <div class="d-flex justify-space-between address-wrapper">
+            <div class="address-text">
+              {{ polkadotAddress.substring(0, 25) }}..
+            </div>
+            <v-btn
+              class="ma-2"
+              text
+              icon
+              color="#5640A5"
+              small
+              @click="handleCopy(polkadotAddress, 'polkadot')"
+            >
+              <div>
+                <v-img src="@/assets/copy-icon.svg" />
+              </div>
+            </v-btn>
+          </div>
+          <div class="text-content">Balance</div>
+          <div class="d-flex">
+            <div class="wallet-icon">
+              <v-img src="@/assets/debio-logo.png" />
+            </div>
+            <b class="notification-subtitle">{{ balance }} DBIO</b>
+          </div>
+        </div>
+        <div class="divider"></div>
+        <div>
+          <div class="d-flex justify-space-between">
+            <div class="d-flex">
+              <div class="wallet-icon">
+                <v-img src="@/assets/metamask-icon.svg" />
+              </div>
+              <b class="notification-subtitle">Metamask</b>
+            </div>
+            <div class="disconnect-text" v-if="metamaskAddress" @click="disconnectWallet">Disconnect</div>
+          </div>
+          <div v-if="metamaskAddress">
+            <div class="text-content">Address</div>
+            <div class="d-flex justify-space-between address-wrapper">
+              <div class="address-text">
+                {{ metamaskAddress.substring(0, 25) }}..
+              </div>
+              <v-btn
+                class="ma-2"
+                text
+                icon
+                color="#5640A5"
+                small
+                @click="handleCopy(metamaskAddress, 'metamask')"
+              >
+                <div>
+                  <v-img src="@/assets/copy-icon.svg" />
+                </div>
+              </v-btn>
+            </div>
+            <div class="text-content">Balance</div>
+            <div class="d-flex">
+              <div class="wallet-icon">
+                <v-img src="@/assets/dai-icon.svg" />
+              </div>
+              <b class="notification-subtitle"
+                >{{ Number(metamaskBalance).toFixed(3) }} DAI</b
+              >
+            </div>
+          </div>
+          <div v-else>
+            <div class="text-content">
+              Wallet not found, Connect Wallet to bind your Metamask wallet to
+              your account
+            </div>
+            <Button class="btn-connect-wallet" @click="openWalletBinding">CONNECT WALLET</Button>
+          </div>
+        </div>
+      </v-card>
+    </div>
+  </v-menu>
+</template>
+
+<script>
+import {mapState, mapMutations, mapActions} from "vuex"
+import {ethAddressByAccountId} from "@/lib/polkadotProvider/query/userProfile"
+import {queryBalance} from "@/lib/polkadotProvider/query/balance"
+import {getBalanceETH} from "@/lib/metamask/wallet.js"
+import {fromEther} from "@/lib/balance-format"
+import Button from "@/components/Button"
+
+export default {
+  name: "HeaderUserInfo",
+  computed: {
+    ...mapState({
+      walletBalance: (state) => state.substrate.walletBalance,
+      api: (state) => state.substrate.api,
+      wallet: (state) => state.substrate.wallet,
+      metamaskWalletAddress: (state) => state.metamask.metamaskWalletAddress,
+      metamaskWalletBalance: (state) => state.metamask.metamaskWalletBalance
+    })
+  },
+  data: () => ({
+    balance: 0,
+    polkadotAddress: "",
+    metamaskAddress: "",
+    metamaskBalance: 0
+  }),
+
+  components: {
+    Button
+  },
+
+  methods: {
+    ...mapMutations({
+      setWalletBalance: "substrate/SET_WALLET_BALANCE",
+      clearWallet: "metamask/CLEAR_WALLET",
+      setMetamaskAddress: "metamask/SET_WALLET_ADDRESS",
+      setMetamaskBalance: "metamask/SET_WALLET_BALANCE"
+    }),
+
+    ...mapActions({
+      clearAuth: "auth/clearAuth"
+    }),
+
+    async getBalance(balanceNummber) {
+      const balance = Number(await fromEther(balanceNummber)).toFixed(3)
+      this.balance = balance.replace(/\.000/, "")
+      if (this.balance == "0") {
+        this.balance = "0"
+      }
+    },
+
+    async fetchWalletBalance() {
+      try {
+        const balanceNummber = await queryBalance(this.api, this.wallet.address)
+        this.getBalance(balanceNummber)
+        this.setWalletBalance(balanceNummber)
+      } catch (err) {
+        console.error(err)
+      }
+    },
+
+    openWalletBinding() {
+      this.$emit("showWalletBinding", true)
+    },
+
+    async checkMetamaskAddress() {
+      let address = ""
+      if (this.metamaskWalletAddress == "") {
+        const ethRegisterAddress = await ethAddressByAccountId(
+          this.api,
+          this.wallet.address
+        )
+        address = ethRegisterAddress
+      } else {
+        address = this.metamaskWalletAddress
+      }
+
+      if (address != "") {
+        const balance = await getBalanceETH(address)
+
+        this.setMetamaskAddress(address)
+        this.setMetamaskBalance(balance)
+        this.metamaskAddress = address
+        this.metamaskBalance = balance
+      }
+    },
+
+    handleCopy(text, wallet) {
+      navigator.clipboard.writeText(text)
+
+      if (wallet === "polkadot") {
+        this.polkadotAddress = "Address Copied!"
+        setTimeout(() => {
+          this.polkadotAddress = text
+        }, 1000)
+      } else if (wallet === "metamask") {
+        this.metamaskAddress = "Address Copied!"
+        setTimeout(() => {
+          this.metamaskAddress = text
+        }, 1000)
+      }
+    },
+
+    disconnectWallet() {
+      this.clearWallet()
+    }
+  },
+
+  watch: {
+    async wallet() {
+      await this.fetchWalletBalance()
+    },
+
+    walletBalance() {
+      this.getBalance(this.walletBalance)
+    }
+  },
+
+  async mounted() {
+    this.polkadotAddress = this.wallet.address
+
+    await this.fetchWalletBalance()
+    await this.checkMetamaskAddress()
+  }
+}
+</script>
+
+<style>
+.card-container {
+  width: 328px;
+  padding: 10px;
+}
+
+.wallet-icon {
+  width: 14px;
+  margin: 12px 0px 0px 10px;
+}
+
+.notification-title {
+  font-size: 16px;
+  display: block;
+  padding: 10px 3px 7px 3px;
+  margin-bottom: 10px;
+  border-bottom: 1px solid #e9e9e9;
+}
+
+.notification-subtitle {
+  font-size: 14px;
+  display: block;
+  padding: 10px 3px 7px 3px;
+  margin: 0 7px 0 7px;
+  color: #595959;
+}
+
+.text-content {
+  font-size: 12px;
+  color: #8c8c8c;
+  margin: 10px 10px 0px 10px;
+}
+
+.address-wrapper {
+  background-color: #f5f5f5;
+  color: #757274 !important;
+  margin: 0px 10px 0px 10px;
+  border-radius: 5px;
+  padding: 0 8px;
+}
+
+.address-text {
+  margin: auto 0px;
+  font-size: 14px;
+}
+
+.disconnect-text {
+  font-size: 12px;
+  display: block;
+  cursor: pointer;
+  padding: 10px 3px 7px 3px;
+  margin: 0 7px 0 7px;
+  color: #f60689;
+}
+
+.btn-connect-wallet {
+  width: auto !important;
+  margin: 15px 10px 15px 10px;
+}
+
+.divider {
+  border-bottom: 1px solid #e9e9e9;
+  margin: 15px 10px 15px 10px;
+}
+</style>
