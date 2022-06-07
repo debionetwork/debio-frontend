82794b9dd0ed266886dec7458c1367623551824f
Dicki Maulana <bukandicki@gmail.com>
2022-06-07 18:53:07 +0700

    feat: Add maintenance page (#435)
    Co-authored-by: Hildegard Lydia <37687466+hilyds@users.noreply.github.com>

--- src/router/routes/lab.js
+++ src/router/routes/lab.js
@@ -20,6 +20,16 @@ const labRoutes = [
         component: () => import(/* webpackChunkName */ "@/views/Dashboard/Lab")
       },
       {
+        path: "maintenance",
+        name: "maintenance",
+        meta: { pageHeader: "Maintenance" },
+        component: () => import(/* webpackChunkName */ "@/views/maintenancePageLayout"),
+        beforeEnter: (to, from, next) => {
+          if (from.path === "/") next({ name: "lab-dashboard" })
+          else next()
+        }
+      },
+      {
         path: "/lab/account",
         name: "lab-dashboard-account",
         meta: {
