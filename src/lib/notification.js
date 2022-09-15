39c0121bb080c06167295d0761a3160b7a5ee5f9
ciniiia <cinia@blocksphere.id>
2022-09-15 09:22:38 +0700

    fix: add reference id format

--- src/lib/notification.js
+++ src/lib/notification.js
@@ -7,6 +7,13 @@ const routes = {
   "Add service": "lab-dashboard-services-detail"
 }
 
+function referenceFormater(id) {
+  if (id?.includes("0x")) {
+    return `${id.slice(0, 4)}...${id.slice(-4)}`
+  }
+  return id
+}
+
 export async function getUnlistedNotification (roles, newBlock, lastBlock) {
   
   const role = roles.replace(roles[0], roles[0].toUpperCase())
@@ -20,37 +27,38 @@ export async function getUnlistedNotification (roles, newBlock, lastBlock) {
   if (listNotificationJson) {
     listNotification = JSON.parse(listNotificationJson)    
   }
+
+  if(lastBlock >= parseInt(data[data.length-1].block_number)) return
   
   data.forEach(event => {
-    if(lastBlock !== parseInt(event.block_number)){
-      const dateSet = new Date(event.created_at)
-      const timestamp = dateSet.getTime().toString()
-      const notifDate = dateSet.toLocaleString("en-US", {
-        weekday: "short", // long, short, narrow
-        day: "numeric", // numeric, 2-digit
-        year: "numeric", // numeric, 2-digit
-        month: "long", // numeric, 2-digit, long, short, narrow
-        hour: "numeric", // numeric, 2-digit
-        minute: "numeric"
-      });
-  
-      const message = event.description.replace("[]", event.reference_id)
-      
-      listNotification.push({
-        message,
-        timestamp,
-        route: routes[event.entity],
-        params: event.reference_id,
-        block: event.block_number,
-        read: false,
-        notifDate
-      })
-  
-      localStorage.setLocalStorageByName(storageName, JSON.stringify(listNotification));
-      listNotification.reverse();
-  
-      store.dispatch("substrate/SET_LIST_NOTIFICATION", listNotification)  
-    }
+    const dateSet = new Date(event.created_at)
+    const timestamp = dateSet.getTime().toString()
+    const notifDate = dateSet.toLocaleString("en-US", {
+      weekday: "short", // long, short, narrow
+      day: "numeric", // numeric, 2-digit
+      year: "numeric", // numeric, 2-digit
+      month: "long", // numeric, 2-digit, long, short, narrow
+      hour: "numeric", // numeric, 2-digit
+      minute: "numeric"
+    });
+
+    const message = event.description.replace("[]", referenceFormater(event.reference_id))
+    
+    listNotification.push({
+      message,
+      timestamp,
+      route: routes[event.entity],
+      params: event.reference_id,
+      block: event.block_number,
+      read: false,
+      notifDate
+    })
+
+    localStorage.setLocalStorageByName(storageName, JSON.stringify(listNotification));
+    listNotification.reverse();
+
+    store.dispatch("substrate/SET_LIST_NOTIFICATION", listNotification)
+    
   });
 }
 
